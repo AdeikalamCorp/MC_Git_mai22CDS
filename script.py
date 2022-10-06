3a871c842aa0de2a92b43ac60dc5f2c7047bdcc2
quanghai92 <statistiques1992@gmail.com>
2022-10-06 18:24:37 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 def fill_list(une_liste):
 	"""
 	une_liste : [10, np.nan, 12, 16, 8, np.nan, 20]
@@ -6,6 +6,13 @@ def fill_list(une_liste):
 	liste_remplie : [10, 11, 12, 16, 8, 14, 20]
 	"""
 
-	#TO DO : Ecrire cette fonction (test)
+	s = pd.Series(une_liste)
+
+	s1=s.ffill()
+	s2=s.bfill()
+
+	s = (s1+s2)/2
+
+	liste_remplie = list(s.values)
 
 	return liste_remplie
