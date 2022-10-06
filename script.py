d9200795e38021d48051062e91d0aa1c6e559139
NordineR <108346306+NordineR@users.noreply.github.com>
2022-10-06 18:17:13 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 
 def fill_list(une_liste):
 	"""
-	une_liste : [10, np.nan, 12, 16, 8, np.nan, 20]
+	une_liste : [10, np.nan, 12, 16, 8, np.nan, 20, 58]
 	
-	liste_remplie : [10, 11, 12, 16, 8, 14, 20]
+	liste_remplie : [10, 11, 12, 16, 8, 14, 20 ,58]
 	"""
 
 	#TO DO : Ecrire cette fonction
